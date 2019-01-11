5e2d3da7b3cac33384561ffcbe3f9cb50f755b13
Alex <17598972+sandpipes@users.noreply.github.com>
2019-01-10 19:30:28 -0500

    Updated link

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ String.prototype.repeat = function(times) {
 };
 
 $( document ).ready(function() {
-    $.getJSON('https://vcmisc.github.io/projects/vsm/allcourses.json', function(data) {
+    $.getJSON('https://vcmisc.github.io/vsm/allcourses.json', function(data) {
         courseDict = data;
     });
     
